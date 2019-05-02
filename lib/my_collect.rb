801b19d39e9214f4ffe0f1553795f5a5f4c6df7e
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-02 00:00:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,9 @@
-def my_collect(empty_array)
-  i = 0 
-end 
-
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
